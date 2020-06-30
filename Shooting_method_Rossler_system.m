85dc759be2bc610ae362a2dd1293e72050a383d9
hassanalkhayuon <hm269hm@gmail.com>
2020-06-30 15:49:29 +0100

    updating the code

--- Shooting_method_Rossler_system.m
+++ Shooting_method_Rossler_system.m
@@ -1,46 +1,5 @@
-% to copute the stable and unstable dairations of \Gamma^+
-
 clear
-
-%% the stable diractions by the Poincaré map ( about 1 mint)
-% par_aut = [0.2 0.2 5.7];
-% T_per   = 5.881088436121394;
-%
-% gamma    =   [-5.242526776906579
-%               -5.242527133727762
-%                0.017632169555805];
-% gammafix =  [gamma(1), gamma(3)];
-% wdot        = @(t,var,T)(T*Rossfun_aut(var,par_aut));
-% M           = @(init)IVP_LE(@(t,var)wdot(t,var,T_per),...
-%               [0 1],init,1e-5);
-% J           = MyJacobian(M,gamma);
-% eigvalue    = eig(J);
-% [eigvect,~] = eig(J);
-% un1         = eigvect(:,1);
-% ce1         = eigvect(:,2);
-% st1         = eigvect(:,3);
-% st          = [st1(1),st1(3)];
-% un          = [un1(1),un1(3)];
-% ce          = [ce1(1),ce1(3)];
-%
-% stref1       = gammafix + (0.0025*(st));
-% stref2       = gammafix - (0.0025*(st));
-% unref1       = gammafix + (0.6*(un));
-% unref2       = gammafix - (0.6*(un));
-%% The periodic orbit \Gamma
-
-% initcond_per =  gamma;
-% ivpinit      = ode45(@(t,var)wdot(t,var,T_per),[0 1],initcond_per);
-% temp         = @(s)(deval(ivpinit,s));
-% si           = linspace(0,1,200);
-% solinit      = bvpinit(si,temp,T_per);
-% BVPper       = per_Ros(par_aut,solinit);
-
-
-%%
-
-%load('detaold\st_un_ce.mat')
-
+clc
 
 %% Shooting on varing r 
 % this section takes between 10 sec up to 3 mints depends on rstep
@@ -117,15 +76,13 @@ set(gcf,'Position',[0.3e3 0.3e3 1e3 0.35e3])
 box on
 
 % saving pdf
-set(FIG_eta,'Units','Inches');
-pos1 = get(FIG_eta,'Position');
-set(FIG_eta,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',...
-    [pos1(3), pos1(4)])
-print(FIG_eta,'Ross_r_vs_del_T155','-dpdf','-r0')
+% set(FIG_eta,'Units','Inches');
+% pos1 = get(FIG_eta,'Position');
+% set(FIG_eta,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',...
+%     [pos1(3), pos1(4)])
+% print(FIG_eta,'Ross_r_vs_del_T155','-dpdf','-r0')
 
 %% Plotting the connection at certain rates Fig~4 in the paper
-
-load('data/eta_movie_155.mat')  % calling stored data
 
 % two values of r for two diffrent connections 
 r             = 0.9202212159423;
@@ -146,8 +103,6 @@ x       = @(tt)interp1(t,var(1,:),tt,'spline');
 y       = @(tt)interp1(t,var(2,:),tt,'spline');
 z       = @(tt)interp1(t,var(3,:),tt,'spline');
 
-% T0 = LastIntersectonTime(t,var,Limits);
-% eta = (([x(T0),z(T0)] - gammafix)*st')/(st*st');
 
 % plotting
 
@@ -157,7 +112,7 @@ hold on
 plot3(var(1,:),var(2,:),var(3,:),'-b','LineWidth',2)
 plot3(initcond_conn(1),initcond_conn(2),...
     initcond_conn(3),'r.','MarkerSize',20)
-plot3(BVPper.y(1,:),BVPper.y(2,:),BVPper.y(3,:),'r-.','LineWidth',3)
+
 xlabel('$x(t)$')
 ylabel('$y(t)$')
 zlabel('$z(t)$','Rotation',0)
@@ -170,12 +125,12 @@ ylim([-15 15]);
 zlim([-1 25]);
 
 % saving pdf
-set(gcf,'Position',[0.04e3 0.04e3 0.68e3 0.6e3])
-set(FIG1,'Units','Inches');
-pos1 = get(FIG1,'Position');
-set(FIG1,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',...
-    [pos1(3), pos1(4)])
-print(FIG1,'Ross_conn_3d_r09202212159423','-dpdf','-r0')
+% set(gcf,'Position',[0.04e3 0.04e3 0.68e3 0.6e3])
+% set(FIG1,'Units','Inches');
+% pos1 = get(FIG1,'Position');
+% set(FIG1,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',...
+%     [pos1(3), pos1(4)])
+% print(FIG1,'Ross_conn_3d_r09202212159423','-dpdf','-r0')
 
 FIG2=figure(3);
 plot(t,var(2,:),'-b','LineWidth',2)
@@ -190,92 +145,12 @@ ylim([-15 15])
 grid on
 
 % saving pdf
-set(gcf,'Position',[0.5e3 0.3e3 0.68e3 0.3e3])
-set(FIG2,'Units','Inches');
-pos2 = get(FIG2,'Position');
-set(FIG2,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',...
-    [pos2(3), pos2(4)])
-print(FIG2,'Ross_conn_2d_r0995651959127','-dpdf','-r0')
-
-%% Movie zeros of eta gives connection
-load('data/eta_movie_155.mat')  % calling stored data 
-figure
-set(gcf,'Color','w')
-V = VideoWriter('conn_vs_r155_20','MPEG-4');
-V.FrameRate = 20;
-open(V);
-
-for nn = 1: length(rr)
-    % interpolation
-    x_conn  = @(tt)interp1(t,X(nn,:),tt,'spline');
-    y_conn  = @(tt)interp1(t,Y(nn,:),tt,'spline');
-    z_conn  = @(tt)interp1(t,Z(nn,:),tt,'spline');
-    sss     = linspace(-10,T0(nn),5000);
-    
-    %subplot1
-    subplot(6,5,[2:4,7:9,13:14])
-    
-    % 1) the periodic orbit;
-    plot3(BVPper.y(1,:),BVPper.y(2,:),BVPper.y(3,:),'-r','LineWidth',2);
-    hold on
-    
-    % 2) the past fixed point;
-    plot3(initcond_conn(1),initcond_conn(2),initcond_conn(3),'r.','MarkerSize',20);
-    
-    % 3) the section;
-    plot3(-8:1:-2,-8:1:-2,zeros(1,7),'-k','LineWidth',3);
-    
-    % 4) the connection;
-    plot3(x_conn(sss),y_conn(sss),z_conn(sss),'b-','LineWidth',1);
-    
-    % 5) the end point of the conniction;
-    plot3(x_conn(T0(nn)),y_conn(T0(nn)),z_conn(T0(nn)),'b.','MarkerSize',20);
-    
-    % 6) the fixed point of the return map (gammafix)
-    plot3(gamma(1),gamma(2),gamma(3),'r.','MarkerSize',20);
-    
-    % 7) Figure settings.
-    set(0,'defaulttextInterpreter','latex')
-    axis([-10 12 -12 10 -0.5 30])
-    yticks([-10  0  10]);
-    xticks([-10 -5 0 5 10]);
-    xlabel('$x$')
-    ylabel('$y$','Rotation',0)
-    set(gca,'FontSize', 22);
-    box on
-    grid on
-    hold off
-    view([10 30])
-    % subplot2
-    subplot(6,5,21:30)
-    
-    % 1) eta and its zeros
-    plot(rr,eta,'b-',rr,zeros(1,length(rr)),'r-','LineWidth',3);
-    hold on
-    
-    % 2) moving horizontal line
-    plot(rr(nn)*ones(1,10),linspace(-0.2,0.4,10),'--','Color',...
-        [0.65 0.65 0.65],'LineWidth',2)
-    
-    % 3) Figure settings
-    xlim([0.9,1]);
-    ylim([-0.2 0.4])
-    set(0,'defaulttextInterpreter','latex')
-    xlabel('$r$')
-    ylabel('$\eta$','Rotation',0)
-    set(gca,'FontSize', 22);
-    set(gcf,'Position',[0.01e3 0.045e3 0.68e3 0.74e3])
-    box on
-    hold off
-    
-    drawnow ()
-    
-    
-    F(nn) = getframe(gcf);
-    writeVideo(V,F(nn));
-end
-close(V);
-winopen('conn_vs_r155_20.mp4')
+% set(gcf,'Position',[0.5e3 0.3e3 0.68e3 0.3e3])
+% set(FIG2,'Units','Inches');
+% pos2 = get(FIG2,'Position');
+% set(FIG2,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',...
+%     [pos2(3), pos2(4)])
+% print(FIG2,'Ross_conn_2d_r0995651959127','-dpdf','-r0')
 
 %% Functions
 
